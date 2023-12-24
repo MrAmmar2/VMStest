76563b15ef23af818e170c9073f49a45f344be55
MrAmmar2 <ammarpauzan@gmail.com>
2023-12-24 11:47:55 +0800

    Update server.js

--- server.js
+++ server.js
@@ -28,19 +28,9 @@ async function run() {
     app.listen(port, () => {
       console.log(`Server listening at http://localhost:${port}`);
     });
-   const express = require('express')
-   const app = express()
-   const port = process.env.PORT || 3000;
-
-   app.use(express.json())
-
-   app.get('/', (req, res) => {
-      res.send('Hello World!')
-   })
-
-   app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`)
-   })
+    app.get('/', (req, res) => {
+       res.send('Hello World!')
+    });
     app.post('/regAdmin', async (req, res) => {
       let data = req.body;
       res.send(await regAdmin(client, data));
