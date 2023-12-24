c1faedd208f37f4643aba1a83375e95223694cd2
MrAmmar2 <ammarpauzan@gmail.com>
2023-12-24 11:04:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,19 @@ const app = express();
 const port = 3000;
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
 
+app.use(express.json())
+
+app.get('/', (req, res) => {
+   res.send('Hello World!')
+})
+
+app.listen(port, () => {
+   console.log(`Example app listening on port ${port}`)
+})
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
