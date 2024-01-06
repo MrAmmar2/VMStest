7ef4c05db698e4d7067fc0a14c92e6cd9ddb60c0
MrAmmar2 <ammarpauzan@gmail.com>
2024-01-06 18:39:14 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -121,6 +121,25 @@ app.post('/BossLogin', async (req, res) => {
   let data = req.body;
   res.send(await Bosslogin(client, data));
 });
+async function Bosslogin(client, data) {
+  const user = await client.db('Database').collection('Boss').findOne({ username: data.username });
+        Admins = await client.db('Database').collection('Admin1').find({role:"Admin"}).next() //.next to read in object instead of array
+        Security = await client.db('Database').collection('Security').find({role:"Security"}).toArray()
+        Visitors = await client.db('Database').collection('PassVisitor').find({role:"Visitor"}).toArray()
+  if (user) {
+    const isPasswordMatch = await decryptPassword(data.password, user.password);
+
+    if (isPasswordMatch) {
+      {
+        return Admins, Security, Visitors
+        }
+    } else {
+      return "Wrong password";
+    }
+  } else {
+    return "User not found";
+  }
+} 
 
 /**
  *  @swagger
@@ -529,15 +548,12 @@ run().catch(console.error);
         const isPasswordMatch = await decryptPassword(data.password, user.password);
     
         if (isPasswordMatch) {
-          if(data.role == 'Boss') {
+          {
             Admins = await client.db('Database').collection('Admin1').find({role:"Admin"}).next() //.next to read in object instead of array
             Security = await client.db('Database').collection('Security').find({role:"Security"}).toArray()
             Visitors = await client.db('Database').collection('PassVisitor').find({role:"Visitor"}).toArray()
             return {Admins, Security, Visitors}
             }
-      
-          
-          
         } else {
           return "Wrong password";
         }
